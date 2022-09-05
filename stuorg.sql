ad4a1ad3c2931316f237c5dbc13d3af9ae717c32
Simon0-0 <97345020+Simon0-0@users.noreply.github.com>
2022-09-05 21:58:29 +0200

    uptade

--- stuorg.sql
+++ stuorg.sql
@@ -18,7 +18,7 @@ displayname NVARCHAR (50) NOT NULL,
 accountdescription NVARCHAR (MAX), 
 FK_userid INT UNIQUE,
 
-CONSTRAINT [not sure what to put here...] FOREIGN KEY (FK_userid) REFERENCES stuorgUser (userid),
+CONSTRAINT [not sure what to put here...] FOREIGN KEY (FK_userid) REFERENCES stuorgUser (userid)
 );
 
 -- PASSWORD
@@ -31,3 +31,42 @@ CONSTRAINT [not sure what to put here...] FOREIGN KEY (FK_accountid) REFERENCES 
 );
 
 
+-- GROUP
+CREATE TABLE stuorgGroup
+(
+groupid INT NOT NULL IDENTITY PRIMARY KEY UNIQUE,
+FK_userid INT UNIQUE,
+groupdescription NVARCHAR(255) NOT NULL
+
+
+CONSTRAINT stuorFK_Group_User FOREIGN KEY (FK_userid) REFERENCES stuorgUser (userid)
+);
+
+
+--GROUP TASK
+CREATE TABLE stuorgGroupTask
+(
+FK_taskid INT UNIQUE,
+FK_groupid INT UNIQUE,
+FK_userid INT UNIQUE,
+
+
+CONSTRAINT stuorFK_GroupTask_User FOREIGN KEY (FK_userid) REFERENCES stuorgUser (userid),
+CONSTRAINT stuorFK_GroupTask_Group FOREIGN KEY (FK_groupid) REFERENCES stuorgGroup (groupid),
+CONSTRAINT stuorFK_GroupTask_Task FOREIGN KEY (FK_taskid) REFERENCES stuorgTask (taskid)
+);
+
+--STUORGUSERGROUP
+
+CREATE TABLE stuorgUserGroup
+(
+FK_groupid INT NOT NULL,
+FK_userid INT NOT NULL,
+CONSTRAINT PK_UserGroup PRIMARY KEY
+    (
+        groupid,
+        userid
+    ),
+FOREIGN KEY (FK_userid) REFERENCES stuorgUser (userid),
+FOREIGN KEY (FK_groupid) REFERENCES stuorgUser (groupid)
+);
