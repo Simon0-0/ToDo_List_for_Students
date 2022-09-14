2b79de674092d13d898c0715f9b948ed83cf0f5d
idarishoj <idarishoj@hotmail.com>
2022-09-14 21:41:49 +0200

    update to tasks.js route

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,8 +5,9 @@ const auth = require('../middleware/authenticate')
 //const authenticate = require('../middleware/authenticate'); - NOT CREATED
 //const admin = require('../middleware/admin'); - NOT CREATED
 
-//GET /api/tasks/  this is a static address, so it comes before :accountid (which is the one with a variable)
+//GET /api/tasks/(?query options) 
 router.get('/', [], async (req, res) => {
+    res.header('Content-type', 'application/json');
     return res.send(JSON.stringify({message: 'GET /api/tasks/'}));
 }); 
 
